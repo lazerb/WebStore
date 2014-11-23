1972b77fac4ab8ebf4c056bf060342aa9a302886
Toviah Botwinik <lazerb_@hotmail.com>
2014-11-23 15:59:31 -0500

    Finished homepage

--- source/header.php
+++ source/header.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
-<html>
 <head>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <table border="0"  style="width:100%">
 	<tr>
-		<th> <a href="index.html">home</a> </th>	
-		<th> <a href="catalogue.html">catalogue</a> </th>
-		<th> <a href="login.html">login</a> </th>
-		<th> <a href="registration.html">registration</a> </th>
+		<th> <a href="index.php">home</a> </th>	
+		<th> <a href="catalogue.php">catalogue</a> </th>
+		<th> <a href="login.php">login</a> </th>
+		<th> <a href="registration.php">registration</a> </th>
 	</tr>
-</html>
+</table>
--- source/index.php
+++ source/index.php
@@ -1,6 +1,23 @@
-<head>
-	<link rel="stylesheet" type="text/css" href="style.css" />
-</head>
-
-<a href="http://google.com">shit.com</a>
-woowoohoshohfodgsdglisd
+<html>
+	<head>
+		<?php include("header.php"); ?>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<br />
+	<br />
+	<body>
+		<center><table style="width:75%">
+		<tr><td>
+			<h1>Spicetopia</h1>
+			<h2>Our mission</h2>
+			<p>
+			Our mission is to supply you with high quality bulk spices.
+			</p>
+			<h2>Who we are</h2>
+			<p>
+			"I am who I am." (Exodus 3:14)
+			</p>
+		</td></tr>
+		</table></center>
+	</body>
+</html>
